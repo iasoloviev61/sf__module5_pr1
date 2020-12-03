b10852927efa424eb1dbf96df5d2178046070f39
root <root@374329-iasoloviev.tmweb.ru>
2020-12-03 16:53:32 +0300

    change wget to curl method

--- getpage.sh
+++ getpage.sh
@@ -9,15 +9,19 @@ fi
 rm -rf src/*
 LANG=( 'br' 'cn' 'cz' 'da' 'de' 'es' 'fr' 'hu' 'it' 'jp' 'nl' 'pl' 'ru' 'tw' )
 LINK="https://www.chiark.greenend.org.uk/~sgtatham/bugs-"
-wget -P src https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
+cd src
+curl -O https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
+#wget -P src https://www.chiark.greenend.org.uk/~sgtatham/bugs.html
 
 if [ -z "$1" ]; then
     for i in ${LANG[@]}
         do
           echo $LINK$i".html"
-          wget -P src $LINK$i".html"
+#          wget -P src $LINK$i".html"
+	   curl -O $LINK$i".html"
     done
 else
-   wget -P src $LINK$1".html"
+#   wget -P src $LINK$1".html"
+    curl -O $LINK$1".html"
 fi
-cp src/bugs.html src/index.html
+cp bugs.html index.html
